6c25f9c8d2e93ad41118d34acfa200fe6c9aada7
Colby Taperts <colbytaperts@gmail.com>
2019-01-05 11:48:55 -0600

    add in client server streaming

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"github.com/ctaperts/grpc-go-course/greet/greetpb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
-	// "net"
 )
 
 func main() {
@@ -22,6 +22,8 @@ func main() {
 	// fmt.Printf("Created client: %f", c)
 	doUnary(c)
 
+	doServerStreaming(c)
+
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -34,7 +36,33 @@ func doUnary(c greetpb.GreetServiceClient) {
 	}
 	res, err := c.Greet(context.Background(), req)
 	if err != nil {
-		log.Fatalf("error while call Greet RPC: %v", err)
+		log.Fatalf("error while calling Greet unary RPC: %v", err)
 	}
 	log.Printf("Response from Greet: %v", res)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting server streaming")
+
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Colby",
+			LastName:  "Taperts",
+		},
+	}
+	resStream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Greet server streaming RPC: %v", err)
+	}
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			// End of stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while reading stream: %v", err)
+		}
+		log.Printf("Response from GreetManyTimes: %v", msg.GetResult())
+	}
 }
