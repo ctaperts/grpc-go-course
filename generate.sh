74b1ff9f8420965f0bfc257ba12258a08b2f007c
Colby Taperts <colbytaperts@gmail.com>
2019-02-10 11:59:50 -0600

    init blog pb

--- generate.sh
+++ generate.sh
@@ -6,3 +6,4 @@ set -ex
 # Setup greet pb file
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
 protoc calculator/calcpb/calc.proto --go_out=plugins=grpc:.
+protoc blog/blogpb/blog.proto --go_out=plugins=grpc:.
