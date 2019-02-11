384c32796e5ad0067a75c5875edd6df85f86d697
Colby Taperts <colbytaperts@gmail.com>
2019-02-10 19:24:59 -0600

    setup create blog req/res

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,29 +1,90 @@
 package main
 
 import (
-	// "context"
+	"context"
 	"fmt"
 	"github.com/ctaperts/grpc-go-course/blog/blogpb"
+	// "github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/bson/primitive"
+	"github.com/mongodb/mongo-go-driver/mongo"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"log"
 	"os"
 	"os/signal"
-	// "google.golang.org/grpc/codes"
 	// "google.golang.org/grpc/credentials"
-	// "google.golang.org/grpc/status"
+	"google.golang.org/grpc/status"
 	// "io"
 	"net"
 	// "strconv"
-	// "time"
+	"time"
 )
 
+var collection *mongo.Collection
+
 type server struct {
+}
+
+func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
+	blog := req.GetBlog()
+
+	data := blogItem{
+		AuthorID: blog.GetAuthorId(),
+		Title:    blog.GetTitle(),
+		Content:  blog.GetContent(),
+	}
+
+	res, err := collection.InsertOne(context.Background(), data)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Internal error: %v", err),
+		)
+	}
+
+	oid, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Could not convert to OID: %v", err),
+		)
+	}
+
+	return &blogpb.CreateBlogResponse{
+		Blog: &blogpb.Blog{
+			Id:       oid.Hex(),
+			AuthorId: blog.GetAuthorId(),
+			Title:    blog.GetTitle(),
+			Content:  blog.GetContent(),
+		},
+	}, nil
+}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
 }
 
 func main() {
 	// if we crash the go code, output file name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	fmt.Println("Blog Service Started")
+
+	client, err := mongo.NewClient("mongodb://localhost:27017")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("mydb").Collection("blog")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
@@ -52,5 +113,7 @@ func main() {
 	s.Stop()
 	fmt.Println("Closing the listener")
 	lis.Close()
+	fmt.Println("Closing mongodb connection")
+	client.Disconnect(context.TODO())
 	fmt.Println("Application is stopped")
 }
